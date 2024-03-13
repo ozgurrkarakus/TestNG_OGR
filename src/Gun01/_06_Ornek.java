01df5777da4b5e285ae55fb059ba460f9e5c3bdd
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-03-13 22:10:26 +0300

    _06_Ornek

--- src/Gun01/_06_Ornek.java
+++ src/Gun01/_06_Ornek.java
@@ -11,23 +11,24 @@ import org.testng.annotations.Test;
 public class _06_Ornek extends BaseDriver {
 
     @Test
-    public void LoginTest(){
+    public void LoginTest() {
         System.out.println("Login Test başladı");
         driver.get("https://opencart.abstracta.us/index.php?route=account/login");
         Tools.Bekle(2);
-
-        WebElement email=driver.findElement(By.id("input-email"));
-        email.sendKeys("testng1@gmail.com");
-
-        WebElement password=driver.findElement(By.id("input-password"));
-        password.sendKeys("123qweasd");
-
-        WebElement loginBtn=driver.findElement(By.xpath("//input[@type='submit']")); //By.cssSelector("[value='Login']")
-        loginBtn.click();
-
-        wait.until(ExpectedConditions.titleIs("My Account"));
+        WebElement email = driver.findElement(By.id("input-email"));
+        email.sendKeys("ozgurkarakus@gmail.com");
+        WebElement password = driver.findElement(By.id("input-password"));
+        password.sendKeys("Ozgur0234");
+        WebElement login = driver.findElement(By.xpath("//input[@type='submit']"));
+        login.click();wait.until(ExpectedConditions.titleIs("My Account"));
         Assert.assertTrue(driver.getTitle().equals("My Account"));
         System.out.println("Login Test bitti");
+
+
     }
 
 }
+
+
+
+
