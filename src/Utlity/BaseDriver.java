e14bef4fb3e2d008c5642f3d1cb890504fb16020
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-03-16 01:45:24 +0300

    BaseDriver

--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -21,7 +21,7 @@ public class BaseDriver {
 
         driver=new ChromeDriver();
 
-        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
